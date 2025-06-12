bc1fba420d049fe20b830a65067f7800b3b3853a
H1sok4 <88634883+H1sok444@users.noreply.github.com>
2025-06-12 14:46:33 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,20 @@
+from flask import Flask, render_template, jsonify
+import json
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    with open('playbooks.json') as f:
+        data = json.load(f)
+    return render_template('index.html', playbooks=data)
+
+@app.route('/api/playbooks')
+def api_playbooks():
+    with open('playbooks.json') as f:
+        data = json.load(f)
+    return jsonify(data)
+
+if __name__ == '__main__':
+    app.run(debug=True)
 
